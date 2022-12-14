1ccc8bdc318af3165a570d69cb7bd0f35f993b1b
mukhriddin-dev <khodievm@gmail.com>
2022-12-14 09:10:06 +0500

    starter app

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.scss";
+
+const index = () => {
+   return (
+      <div>
+         <h1>Home</h1>
+      </div>
+   );
+};
+
+export default index;<h1>Home</h1>
